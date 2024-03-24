537400467301a4e802cbc446d1ac015348d77d40
Mike Chabot <mike.chabot@live.com>
2024-03-24 01:50:55 -0400

    Explcitly target node14

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export function modernConfig(opts: any) {
   return {
     entry: opts.entry,
     format: ['cjs', 'esm'],
-    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
+    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77', 'node14'],
     outDir: 'dist/modern',
     dts: true,
     sourcemap: true,
@@ -16,7 +16,7 @@ export function legacyConfig(opts: any) {
   return {
     entry: opts.entry,
     format: ['cjs', 'esm'],
-    target: ['es2020', 'node18'],
+    target: ['es2020', 'node14'],
     outDir: 'dist/legacy',
     dts: true,
     sourcemap: true,
